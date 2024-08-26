0ebc218ac1ef5eaca38a61e16e0f638d3b545d06
doueov <gitpdyeon@gmail.com>
2024-08-27 08:57:07 +0900

    Image 컴포넌트

---
+++ src/app/next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+    images: {
+        // 모든 도메인을 통해 접근하는 이미지 표시 허용
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+};
+
+export default nextConfig;
