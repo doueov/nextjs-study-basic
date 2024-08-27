3118780fa55b73023256c2dea6b0974ceff121b2
doueov <gitpdyeon@gmail.com>
2024-08-27 09:48:19 +0900

    layout 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,11 @@
-import "@/app/globals.css"
+import UseRouterDemoComponent from "@/components/UseRouterDemoComponent"
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <header>Header</header>
-        <hr />
+        <UseRouterDemoComponent />
         <main>{children}</main>
-        <hr />
-        <footer>Footer</footer>
       </body>
     </html>
   )
